3e76f5b1eb05ad06573ddec24f158323c997e6ae
ArseneBrosy <arsene.brosy@divtec.ch>
2023-09-26 15:06:01 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 /**
- * @author Steve Fallet
- * @since 2022.09.06
+ * @author Arsène Brosy
+ * @since 2023.09.26
  */
 
 'use strict';
 
-console.log('Test liaison JS');
+const body = document.querySelector("body");
+const listeBg = body.querySelector("#bg")
+
+// Change l'arrière-plan
+body.style.backgroundImage = `url(./img/backgounds/${listeBg.value})`;
+listeBg.addEventListener("change", () => {
+  body.style.backgroundImage = `url(./img/backgounds/${listeBg.value})`;
+});
+
+
+const formulaire = document.querySelector("form");
+const txtPassword = formulaire.querySelector("#password")
+const txtPasswordConfirm = formulaire.querySelector("#confirm-password")
+
+formulaire.addEventListener("submit", () => {
+  if (txtPassword.value !== txtPasswordConfirm.value) {
+    alert("les mots de passe ne correspondent pas");
+    return;
+  }
+  alert("Compte crée avec succès");
+});
