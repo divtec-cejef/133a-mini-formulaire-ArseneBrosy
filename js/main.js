d7c109a1f4be1f8ad1784f05ebdfb0c719905207
ArseneBrosy <arsene.brosy@divtec.ch>
2023-09-26 15:30:16 +0200

    test

--- js/main.js
+++ js/main.js
@@ -19,10 +19,22 @@ const formulaire = document.querySelector("form");
 const txtPassword = formulaire.querySelector("#password")
 const txtPasswordConfirm = formulaire.querySelector("#confirm-password")
 
-formulaire.addEventListener("submit", () => {
+formulaire.addEventListener("submit", (e) => {
+  // Stopper l'envoi ou annuler événement
+  e.preventDefault();
+
+  // Si le mot de passe est plus petit que 3 caractères
+  if (txtPassword.value.length < 3) {
+    alert("Le mot de passe doit contenir au moins 3 caractères");
+    return;
+  }
+
+  // Si les mots de passe ne sont pas identiques
   if (txtPassword.value !== txtPasswordConfirm.value) {
     alert("les mots de passe ne correspondent pas");
     return;
   }
+
   alert("Compte crée avec succès");
+  formulaire.submit();
 });
